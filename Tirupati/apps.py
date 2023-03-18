c3107005af11f215fc4172ca5b74010fd1945174
Rajakuruva <kuruvarajasekhar2019@gmail.com>
2023-03-18 19:48:25 +0530

    first commit

---
+++ Tirupati/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TirupatiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Tirupati'
---
+++ Tirupati/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Tpt1(request):
+    return render(request,'Tpt1.html')
